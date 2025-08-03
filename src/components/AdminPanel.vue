9e1d0484644555a0b8c3835d4d99eceeaac1da34
Igor Pavlić <76666559+igorpavlic@users.noreply.github.com>
2025-08-03 08:48:25 +0200

    Code correction

--- src/components/AdminPanel.vue
+++ src/components/AdminPanel.vue
@@ -1,19 +1,19 @@
 <template>
     <div class="admin-panel">
         <a href="#" @click.prevent="showForm = !showForm">
-            {{ showForm ? '❌ Close' : '➕ Add Words' }}
+            {{ showForm ? '❌ Close Add Words' : '➕ Add Words' }}
         </a>
   
       <div v-if="showForm" class="form-popup">
         <textarea
-          v-model="bulkWords"
+          v-model="Words2Add"
           rows="6"
           cols="40"
           placeholder="like: dog, car, ball, ..."
         ></textarea>
         <br />
-        <button @click="addMultiple">Add Words</button>
-        <p style="color: green" v-if="bulkAddSuccess">✅ Words Added!</p>
+        <button @click="Words2Add">Add Words</button>
+        <p style="color: green" v-if="Words2AddSuccess">✅ Words Added!</p>
       </div>
     </div>
   </template>
@@ -25,22 +25,22 @@
   
   const emit = defineEmits(['refresh'])
   
-  const bulkWords = ref('')
-  const bulkAddSuccess = ref(false)
+  const Words2Add = ref('')
+  const Words2AddSuccess = ref(false)
   const showForm = ref(false)
   
-  const addMultiple = async () => {
-    let words = bulkWords.value
+  const addWords = async () => {
+    let words = Words2Add.value
       .split(/[\s,]+/)
       .map(w => w.toLowerCase().trim())
       .filter(w => w.length > 1)
     words = [...new Set(words)]
     const batch = words.map(w => addDoc(collection(db, 'words'), { word: w }))
     await Promise.all(batch)
-    bulkWords.value = ''
-    bulkAddSuccess.value = true
+    Words2Add.value = ''
+    Words2AddSuccess.value = true
     emit('refresh')
-    setTimeout(() => (bulkAddSuccess.value = false), 2000)
+    setTimeout(() => (Words2AddSuccess.value = false), 2000)
   }
   </script>
   
